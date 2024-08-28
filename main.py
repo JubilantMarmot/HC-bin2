f0ace8322d59139d432de4d328a32bc318ccdf52
JubilantMarmot <abboudalomari6@gmail.com>
2024-08-28 07:26:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from machine import ADC, Pin
 import time
 import math
+import tm1637
 
 ntc_pin = Pin(26)
 adc = ADC(0)
 
+display = tm1637.TM1637(clk=machine.Pin(16), dio=machine.Pin(17))
+
+r = Pin(2, Pin.OUT)
+g = Pin(1, Pin.OUT)
+b = Pin(0, Pin.OUT)
+
+def set_color(red, green, blue):
+    r.value(red)
+    g.value(green)
+    b.value(blue)
+
 def read_temperature():
-    #Source: https://docs.wokwi.com/parts/wokwi-ntc-temperature-sensor
     adc_value = adc.read_u16()
     voltage = adc_value * 3.3 / 65535
 
@@ -23,6 +34,16 @@ def read_temperature():
     return temperature_celsius
 
 while True:
-    temperature = read_temperature()
-    print("Temperature: {:.2f} °C".format(temperature))
+    temperature = int(read_temperature())
+    display.temperature(temperature)
+
+    if temperature < 30:
+        set_color(0, 1, 0)
+    elif temperature < 29:
+        set_color(1, 1, 0)
+    elif temperature < 40:
+        set_color(1, 0, 0)
+    else:
+        set_color(1, 0, 0)
+
     time.sleep(1)
